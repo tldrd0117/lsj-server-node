6e708cfbe878cd78adba7ebde1eb5bed3462dfa0
tldrd0117 <tldrd0117@gmail.com>
2019-01-06 16:17:44 +0900

    better2

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const post = require('./db/query/post');
 
 const bodyParser = require('body-parser');
 
+const port = process.env.PORT || 3000;
+
 app.get('/', function(req, res){
     // res.send('Hello World');
     res.json({ info: 'main' })
 });
 app.get('/post', post.getPost)
 
-app.listen(3000, function() {
+app.listen(port, function() {
     console.log('Example app listening on port 3000!');
 })
