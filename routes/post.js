4b43b59b50363ebfe587d704d0a7001c8fc7c345
tldrd0117 <tldrd0117@gmail.com>
2019-02-10 14:53:43 +0900

    easy

--- routes/post.js
+++ routes/post.js
@@ -18,9 +18,9 @@ const tr = (func) => async (req, res) => {
     }
     
 }
-
+//to_char(create_time, 'YYYY-MM-DD HH24:MI:SS') as create_time
 const selectAll = (type)=>({
-    text: "SELECT title, body, id, to_char(create_time, 'YYYY-MM-DD HH24:MI:SS') as create_time, author, tags FROM Post where type=$1 ORDER BY id DESC",
+    text: "SELECT title, body, id, create_time, author, tags FROM Post where type=$1 ORDER BY id DESC",
     values: [type]
 });
 const insert = (obj) => ({
