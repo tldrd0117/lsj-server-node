dbc4b94bd7dea43a664aba493a7344aff6ac00d7
tldrd0117 <tldrd0117@gmail.com>
2019-03-17 15:45:40 +0900

    user

--- routes/user/index.js
+++ routes/user/index.js
@@ -4,7 +4,7 @@ const {router, passport} = require('./auth');
 
 module.exports = router;
 
-router.post('/user/login', passport.authenticate('local', {},
+router.post('/login', passport.authenticate('local', {},
     function(req, res){
         console.log(req)
         res.status(200).json(req.user);
