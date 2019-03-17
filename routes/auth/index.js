64c7e14c786faf4f9822fd058bbe317a37880b85
tldrd0117 <tldrd0117@gmail.com>
2019-03-17 21:57:26 +0900

    user

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -7,6 +7,7 @@ router.post('/login', function(req, res, next){
     passport.authenticate('local',
     function(err, user, info){
         if (err) { return next(err); }
+        console.log('auth', user, info);
         if (!user) { return res.status(500).send('no user')}
         req.login(user, function(err){
             if(err) return next(err);
