191994ee74f04ea936f9eee3984d833a6b7599ad
tldrd0117 <tldrd0117@gmail.com>
2019-03-17 21:03:10 +0900

    user

--- routes/client.js
+++ routes/client.js
@@ -66,7 +66,7 @@ class Client {
                 res.status(200).json(rows);
             } else {
                 res.locals.rows = rows;
-                return Promise.resolve('next');
+                next(rows);
             }
         },sub)
     }
